9892f35e9c421bf8d4e177a2d4c1b81527f9f22c
James Percival <jpercival@unomaha.edu>
2017-03-26 17:48:12 -0500

    pushing initial files

---
+++ app/src/main/java/com/example/root/test/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.root.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick (View view) {
+        EditText input = (EditText) findViewById(R.id.main_input);
+        String string = input.getText().toString();
+        Toast.makeText(this,string,Toast.LENGTH_LONG).show();
+    }
+}
